767dece8d97f834907f1c016c84439156f3b39ee
aik <aik>
2017-09-25 17:04:24 +0100

    committed

--- src/D3D12HeterogeneousMultiadapter.cpp
+++ src/D3D12HeterogeneousMultiadapter.cpp
@@ -66,8 +66,37 @@ HRESULT D3D12HeterogeneousMultiadapter::GetHardwareAdapters(IDXGIFactory2* pFact
 	// it is the adapter that performs the first set of operations (rendering triangles)
 	// in this sample.
 
-	ThrowIfFailed(pFactory->EnumAdapters1(0, ppSecondaryAdapter));
-	ThrowIfFailed(pFactory->EnumAdapters1(1, ppPrimaryAdapter));
+	ComPtr<IDXGIAdapter1> adapter;
+	UINT32 adapter_idx = 0;
+
+	for (UINT adapterIndex = 0; DXGI_ERROR_NOT_FOUND != pFactory->EnumAdapters1(adapterIndex, &adapter); ++adapterIndex)
+	{
+		DXGI_ADAPTER_DESC1 desc;
+		adapter->GetDesc1(&desc);
+
+		if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+		{
+			// Don't select the Basic Render Driver adapter.
+			// If you want a software adapter, pass in "/warp" on the command line.
+			continue;
+		}
+
+		// Check to see if the adapter supports Direct3D 12, but don't create the
+		// actual device yet.
+		if (SUCCEEDED(D3D12CreateDevice(adapter.Get(), D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr)))
+		{
+			if (adapter_idx == 0)
+			{
+				*ppPrimaryAdapter = adapter.Detach();
+				adapter_idx++;
+			}
+			else if (adapter_idx == 1)
+			{
+				*ppSecondaryAdapter = adapter.Detach();
+				adapter_idx++;
+			}
+		}
+	}
 
 	return S_OK;
 }
@@ -150,22 +179,22 @@ void D3D12HeterogeneousMultiadapter::LoadPipeline()
 	// This sample does not support fullscreen transitions.
 	ThrowIfFailed(factory->MakeWindowAssociation(Win32Application::GetHwnd(), DXGI_MWA_NO_ALT_ENTER));
 
-	for (UINT i = 0; i < GraphicsAdaptersCount; i++)
-	{
-		ComPtr<IDXGISwapChain1> swapChain;
-		ThrowIfFailed(factory->CreateSwapChainForHwnd(
-			m_directCommandQueues[i].Get(),		// Swap chain needs the queue so that it can force a flush on it.
-			Win32Application::GetHwnd(),
-			&swapChainDesc,
-			nullptr,
-			nullptr,
-			&swapChain
-		));
+	ComPtr<IDXGISwapChain1> swapChain;
+	ThrowIfFailed(factory->CreateSwapChainForHwnd(
+		m_directCommandQueues[Secondary].Get(),		// Swap chain needs the queue so that it can force a flush on it.
+		Win32Application::GetHwnd(),
+		&swapChainDesc,
+		nullptr,
+		nullptr,
+		&swapChain
+	));
 
-		ThrowIfFailed(swapChain.As(&m_swapChain));
-	}
+	// This sample does not support fullscreen transitions.
+	ThrowIfFailed(factory->MakeWindowAssociation(Win32Application::GetHwnd(), DXGI_MWA_NO_ALT_ENTER));
 
+	ThrowIfFailed(swapChain.As(&m_swapChain));
 	m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+
 
 	// Create descriptor heaps.
 	{
